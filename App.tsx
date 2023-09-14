3584c294bf0034fa9c0799029bc22daad34bdfb3
vishwa prasanth <109272835+vishwaprasanth@users.noreply.github.com>
2023-09-14 11:21:33 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+    View,
+    Text,
+    SafeAreaView
+} from "react-native";
+
+function App(){
+    return(
+        <SafeAreaView>
+        <View>
+            <Text> Hello world !!</Text>
+            <Text> Hello world !!</Text>
+            <Text> Hello world !!</Text>
+            <Text> Hello world !!</Text>
+            
+        </View>
+    </SafeAreaView>;
+    );
+}
+
+export default App;
